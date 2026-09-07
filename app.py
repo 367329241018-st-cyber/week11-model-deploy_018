42291e81985dbc623f8ab521c3c80cb347d28f94
มาง้ออ่ะ <367329241018-st@rmutsb.ac.th>
2026-09-07 16:07:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ import streamlit as st
 import joblib
 import io
 import os
+import Orange
 
 # ต้องติดตั้งไลบรารี Orange3 (ดู requirements.txt) จึงจะ import ได้
 from Orange.data import Domain, Table
